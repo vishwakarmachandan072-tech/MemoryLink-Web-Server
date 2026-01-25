ef92b403d929debf21e0959b4715178b6f6fa47a
vishwakarmachandan072-tech <vishwakarmachandan072@gmail.com>
2026-01-25 19:04:55 +0530

    fixing vercel prod issues-02

--- actions.ts
+++ actions.ts
@@ -3,21 +3,24 @@ const postUsers = async (email: string) => {
     try {
         console.log("Api: ", `${process.env.WEB_PUBLIC_BACKEND_API_URL}/auth/join-waitlist`);
         const response = await fetch(`${process.env.WEB_PUBLIC_BACKEND_API_URL}/auth/join-waitlist`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+            })
         })
-    })
-    const data = await response.json();
-    if (!response.ok) throw new Error(data.message || 'Something went wrong');
+        const data = await response.json();
+        if (!response.ok) return {
+        success: false,
+        message: data.message || "Something went wrong",
+      };
 
-    return { success: true, message: data.message || "Something went wrong" }
+        return { success: true, message: data.message }
     } catch (error) {
         console.log(error)
-            return { success: false, error }
+        return { success: false, error }
     }
 }
 
